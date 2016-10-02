32bb7ed47fbf45f1b97b3c4d1fb1deffdcaaa372
Matthew <mwar136@aucklanduni.ac.nz>
2016-10-03 10:16:30 +1300

    Added zero to pi function and changed name structure of functions

--- WrapRadians.py
+++ WrapRadians.py
@@ -1,7 +1,10 @@
 import numpy as np
 
-def wrapto2pi(radians):
+def zeroto2pi(radians):
     return radians - 2*np.pi*np.floor(radians/(2*np.pi))
 
-def wraptopi(radians):
+def pitopi(radians):
+    return radians - 2*np.pi*np.floor((radians+np.pi)/(2*np.pi))
+
+def zerotopi(radians):
     return radians - np.pi*np.floor(radians/(np.pi))
